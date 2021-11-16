870a291bc0783469054eb67a5575f66485d31b55
manleebk <levanman1997@gmail.com>
2021-11-16 21:49:51 +0700

    qa api route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AdminController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\QAController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,5 +23,12 @@ Route::group(['middleware' => 'auth:api'], function () {
     Route::post('logout', [AdminController::class, 'logout']);
     Route::group(['prefix' => 'admins'], function () {
         Route::get('', [AdminController::class, 'index']);
+    });
+    Route::group(['prefix' => 'qas'], function () {
+        Route::get('', [QAController::class, 'index']);
+        Route::get('{id}', [QAController::class, 'show']);
+        Route::put('{id}', [QAController::class, 'update']);
+        Route::post('', [QAController::class, 'store']);
+        Route::delete('{id}', [QAController::class, 'destroy']);
     });
 });
