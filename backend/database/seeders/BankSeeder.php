91e8960527e96c988c8dfb877d242c8c282b973c
manleebk <levanman1997@gmail.com>
2021-11-22 20:57:58 +0700

    create seed

---
+++ backend/database/seeders/BankSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Bank;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class BankSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('banks')->truncate();
+        Bank::factory()->count(10)->create();
+    }
+}
