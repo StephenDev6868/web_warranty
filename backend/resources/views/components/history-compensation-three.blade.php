b344eb8c90eed54cbd846e94deeb373906e7d178
manleebk <levanman1997@gmail.com>
2021-12-11 20:38:45 +0700

    minor fix

--- backend/resources/views/components/history-compensation-three.blade.php
+++ backend/resources/views/components/history-compensation-three.blade.php
@@ -5,13 +5,13 @@ $name = 'wrapper';
     <h2 class="title">Lịch sử bồi thường</h2>
     <div class="menu-select d-flex">
         <div class="menu-item menu-account ">
-            <a href="{{ route('history-compensation-one') }}">Thông tin chung</a>
+            <a href="{{ route('history-compensation-one', ['id' => $id]) }}">Thông tin chung</a>
         </div>
         <div class="menu-item menu-history">
-            <a href="{{ route('history-compensation-two') }}">Quá trình giải quyết</a>
+            <a href="{{ route('history-compensation-two', ['id' => $id]) }}">Quá trình giải quyết</a>
         </div>
         <div class="menu-item menu-history active">
-            <a href="{{ route('history-compensation-three') }}">Hồ sơ giấy tờ</a>
+            <a href="{{ route('history-compensation-three', ['id' => $id]) }}">Hồ sơ giấy tờ</a>
         </div>
     </div>
     <!-- info -->
